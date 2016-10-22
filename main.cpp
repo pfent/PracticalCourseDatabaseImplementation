7273b538b58618223a88569d6a62b0bc7a3a3280
pfent <philipp@fent.de>
2016-10-22 20:43:08 +0200

    add a small unit test

--- main.cpp
+++ main.cpp
@@ -6,27 +6,30 @@
 
 using namespace std;
 
+constexpr auto iterations = 30;
+
 int main() {
     using namespace string_literals;
-    auto & database = Database::instance();
-    database.importDatabaseFromPath("../tbls/"s);
+    auto& db = Database::instance();
+    db.importDatabaseFromPath("../tbls/"s);
 
     cout << "Imported database:" << '\n';
-    cout << "NewOrders lines: " << database.newOrders.getView().size() << '\n';
-    cout << "Orders lines: " << database.orders.getView().size() << '\n';
-    cout << "OrderLines lines: " << database.orderLines.getView().size() << std::endl;
+    cout << "NewOrders lines: " << db.newOrders.getView().size() << '\n';
+    cout << "Orders lines: " << db.orders.getView().size() << '\n';
+    cout << "OrderLines lines: " << db.orderLines.getView().size() << std::endl;
 
     auto start = std::chrono::steady_clock::now();
-    for(int i = 0; i < 1'000'000; ++i) {
+    for (int i = 0; i < iterations; ++i) {
         newOrderRandom();
     }
     auto end = std::chrono::steady_clock::now();
-    auto ms = std::chrono::duration<double, std::milli>(end-start).count();
+    auto ms = std::chrono::duration<double, std::milli>(end - start).count();
     cout << ms << "ms spent" << '\n';
-    cout << 1'000'000 / (ms / 1'000)  << " Transactions per second" << '\n';
+    cout << iterations / (ms / 1'000)  << " Transactions per second" << '\n';
     cout << "Database after a million random Orders" << '\n';
-    cout << "NewOrders lines: " << database.newOrders.getView().size() << '\n';
-    cout << "Orders lines: " << database.orders.getView().size() << '\n';
-    cout << "OrderLines lines: " << database.orderLines.getView().size() << std::endl;
+    cout << "NewOrders lines: " << db.newOrders.getView().size() << '\n';
+    cout << "Orders lines: " << db.orders.getView().size() << '\n';
+    cout << "OrderLines lines: " << db.orderLines.getView().size() << std::endl;
+
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <string>
+#include "neworderrandom.h"
+#include "Database.h"
+
+using namespace std;
+
+template <typename T, typename Func>
+void testIndexIntegrity(Table<T> table, Func&& assertion) {
+    for (auto& elem : table.getView()) {
+        auto key = elem.getKey();
+        auto elemTest = table.getElemForKey(key);
+        assert(assertion(elem, elemTest));
+    }
+}
+
+void testIndices(Database& db);
+
+int main() {
+    using namespace string_literals;
+    auto& db = Database::instance();
+    db.importDatabaseFromPath("../tbls/"s);
+
+    testIndices(db);
+
+    for (int i = 0; i < 1'000'000; ++i) {
+        newOrderRandom();
+    }
+
+    testIndices(db);
+
+    cout << "Everything looking good" << endl;
+
+    return 0;
+}
+
+void testIndices(Database& db) {
+    testIndexIntegrity(db.wareHouses, [](auto a, auto b) {
+        return
+            a.w_id == b.w_id &&
+            a.w_name == b.w_name &&
+            a.w_street_1 == b.w_street_1 &&
+            a.w_street_2 == b.w_street_2 &&
+            a.w_city == b.w_city &&
+            a.w_state == b.w_state &&
+            a.w_zip == b.w_zip &&
+            a.w_tax == b.w_tax &&
+            a.w_ytd == b.w_ytd;
+    });
+    testIndexIntegrity(db.districts, [](auto a, auto b) {
+        return
+            a.d_id == b.d_id &&
+            a.d_w_id == b.d_w_id &&
+            a.d_name == b.d_name &&
+            a.d_street_1 == b.d_street_1 &&
+            a.d_street_2 == b.d_street_2 &&
+            a.d_city == b.d_city &&
+            a.d_state == b.d_state &&
+            a.d_zip == b.d_zip &&
+            a.d_tax == b.d_tax &&
+            a.d_ytd == b.d_ytd &&
+            a.d_next_o_id == b.d_next_o_id;
+    });
+    testIndexIntegrity(db.customers, [](auto a, auto b) {
+        return
+            a.c_id == b.c_id &&
+            a.c_d_id == b.c_d_id &&
+            a.c_w_id == b.c_w_id &&
+            a.c_first == b.c_first &&
+            a.c_middle == b.c_middle &&
+            a.c_last == b.c_last &&
+            a.c_street_1 == b.c_street_1 &&
+            a.c_street_2 == b.c_street_2 &&
+            a.c_city == b.c_city &&
+            a.c_state == b.c_state &&
+            a.c_zip == b.c_zip &&
+            a.c_phone == b.c_phone &&
+            a.c_since == b.c_since &&
+            a.c_credit == b.c_credit &&
+            a.c_credit_lim == b.c_credit_lim &&
+            a.c_discount == b.c_discount &&
+            a.c_balance == b.c_balance &&
+            a.c_ytd_paymenr == b.c_ytd_paymenr &&
+            a.c_payment_cnt == b.c_payment_cnt &&
+            a.c_delivery_cnt == b.c_delivery_cnt &&
+            a.c_data == b.c_data;
+    });
+    testIndexIntegrity(db.newOrders, [](auto a, auto b) {
+        return
+            a.no_d_id == b.no_d_id &&
+            a.no_o_id == b.no_o_id &&
+            a.no_w_id == b.no_w_id;
+    });
+    testIndexIntegrity(db.orders, [](auto a, auto b) {
+        return
+            a.o_id == b.o_id &&
+            a.o_d_id == b.o_d_id &&
+            a.o_w_id == b.o_w_id &&
+            a.o_c_id == b.o_c_id &&
+            a.o_entry_d == b.o_entry_d &&
+            a.o_carrier_id == b.o_carrier_id &&
+            a.o_ol_cnt == b.o_ol_cnt &&
+            a.o_all_local == b.o_all_local;
+
+
+    });
+    testIndexIntegrity(db.orderLines, [](auto a, auto b) {
+        return
+            a.ol_o_id == b.ol_o_id &&
+            a.ol_d_id == b.ol_d_id &&
+            a.ol_w_id == b.ol_w_id &&
+            a.ol_number == b.ol_number &&
+            a.ol_i_id == b.ol_i_id &&
+            a.ol_supply_w_id == b.ol_supply_w_id &&
+            a.ol_delivery_d == b.ol_delivery_d &&
+            a.ol_quantity == b.ol_quantity &&
+            a.ol_amount == b.ol_amount &&
+            a.ol_dist_info == b.ol_dist_info;
+    });
+    testIndexIntegrity(db.items, [](auto a, auto b) {
+        return
+            a.i_id == b.i_id &&
+            a.i_im_id == b.i_im_id &&
+            a.i_name == b.i_name &&
+            a.i_price == b.i_price &&
+            a.i_data == b.i_data;
+
+    });
+    testIndexIntegrity(db.stocks, [](auto a, auto b) {
+        return
+            a.s_i_id == b.s_i_id &&
+            a.s_w_id == b.s_w_id &&
+            a.s_quantity == b.s_quantity &&
+            a.s_dist_01 == b.s_dist_01 &&
+            a.s_dist_02 == b.s_dist_02 &&
+            a.s_dist_03 == b.s_dist_03 &&
+            a.s_dist_04 == b.s_dist_04 &&
+            a.s_dist_05 == b.s_dist_05 &&
+            a.s_dist_06 == b.s_dist_06 &&
+            a.s_dist_07 == b.s_dist_07 &&
+            a.s_dist_08 == b.s_dist_08 &&
+            a.s_dist_09 == b.s_dist_09 &&
+            a.s_dist_10 == b.s_dist_10 &&
+            a.s_ytd == b.s_ytd &&
+            a.s_order_cnt == b.s_order_cnt &&
+            a.s_remote_cnt == b.s_remote_cnt &&
+            a.s_data == b.s_data;
+    });
+}
+
