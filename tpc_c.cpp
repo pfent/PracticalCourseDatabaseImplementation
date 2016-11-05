13ffd167153858e575481e46198ea0c3c7f11756
pfent <philipp@fent.de>
2016-11-05 16:51:11 +0100

    really make use of the column store, no sense getting all solumns every time

--- tpc_c.cpp
+++ tpc_c.cpp
@@ -8,14 +8,13 @@
 using namespace std;
 void newOrder(int32_t w_id, int32_t d_id, int32_t c_id, int32_t items, int32_t supware[], int32_t itemid[], int32_t qty[], Timestamp datetime) {
     auto& db = Database::instance();
-    const auto w_tax = db.warehouse.getRowForKey(w_id).w_tax;
-    const auto c_discount = db.customer.getRowForKey(make_tuple(w_id, d_id, c_id)).c_discount;
+    const auto w_tax = db.warehouse.w_tax[db.warehouse.primaryHashIndex[ {w_id}]];
+    const auto c_discount = db.customer.c_discount[db.customer.primaryHashIndex[ {w_id, d_id, c_id}]];
 
-    auto districtLine = db.district.getRowForKey(make_tuple(w_id, d_id));
-    const auto o_id = districtLine.d_next_o_id;
-    const auto d_tax = districtLine.d_tax;
-    districtLine.d_next_o_id = o_id + 1;
-    db.district.update(districtLine);
+    const auto districtIndex = db.district.primaryHashIndex[ {w_id, d_id}];
+    const auto o_id = db.district.d_next_o_id[districtIndex];
+    const auto d_tax = db.district.d_tax[districtIndex];
+    db.district.d_next_o_id[districtIndex] = o_id + 1;
 
     const auto all_local = all_of(supware, supware + items, [&](int32_t item) {
         return w_id == item;
@@ -23,43 +22,43 @@ void newOrder(int32_t w_id, int32_t d_id, int32_t c_id, int32_t items, int32_t s
     db.order.insert({o_id, d_id, w_id, c_id, datetime, 0, items, all_local});
     db.neworder.insert({o_id, d_id, w_id});
     for (int index = 0; index < items; ++index) {
-        const auto i_price = db.item.getRowForKey(itemid[index]).i_price;
-        const auto stockLine = db.stock.getRowForKey(make_tuple(supware[index], itemid[index]));
-        const auto s_quantity = stockLine.s_quantity;
-        const auto s_remote_cnt = stockLine.s_remote_cnt;
-        const auto s_order_cnt = stockLine.s_order_cnt;
-        const auto s_dist = [&stockLine, d_id]() {
+        const auto i_price = db.item.i_price[db.item.primaryHashIndex[itemid[index]]];
+        const auto stockIndex = db.stock.primaryHashIndex[ {supware[index], itemid[index]}];
+        const auto s_quantity = db.stock.s_quantity[stockIndex];
+        const auto s_remote_cnt = db.stock.s_remote_cnt[stockIndex];
+        const auto s_order_cnt = db.stock.s_order_cnt[stockIndex];
+        const auto s_dist = [&]() {
             switch (d_id) {
             default:
-            case  1: return stockLine.s_dist_01;
-            case  2: return stockLine.s_dist_02;
-            case  3: return stockLine.s_dist_03;
-            case  4: return stockLine.s_dist_04;
-            case  5: return stockLine.s_dist_05;
-            case  6: return stockLine.s_dist_06;
-            case  7: return stockLine.s_dist_07;
-            case  8: return stockLine.s_dist_01;
-            case  9: return stockLine.s_dist_09;
-            case 10: return stockLine.s_dist_10;
+            case  1: return db.stock.s_dist_01[stockIndex];
+            case  2: return db.stock.s_dist_02[stockIndex];
+            case  3: return db.stock.s_dist_03[stockIndex];
+            case  4: return db.stock.s_dist_04[stockIndex];
+            case  5: return db.stock.s_dist_05[stockIndex];
+            case  6: return db.stock.s_dist_06[stockIndex];
+            case  7: return db.stock.s_dist_07[stockIndex];
+            case  8: return db.stock.s_dist_01[stockIndex];
+            case  9: return db.stock.s_dist_09[stockIndex];
+            case 10: return db.stock.s_dist_10[stockIndex];
             }
         }();
         {
-            auto stockLine = db.stock.getRowForKey(make_tuple(supware[index], itemid[index]));
+            const auto toUpdate = db.stock.primaryHashIndex[ {supware[index], itemid[index]}];
+            auto updateValue = s_quantity;
             if (s_quantity > qty[index]) {
-                stockLine.s_quantity = s_quantity - qty[index];
+                updateValue = updateValue - qty[index];
             } else {
-                stockLine.s_quantity = s_quantity + 91 - qty[index];
+                updateValue += 91 - qty[index];
             }
-            db.stock.update(stockLine);
+            db.stock.s_quantity[toUpdate] = updateValue;
         }
         {
-            auto stockLine = db.stock.getRowForKey(make_tuple(w_id, itemid[index]));
+            const auto toUpdate = db.stock.primaryHashIndex[ {w_id, itemid[index]}];
             if (supware[index] != w_id) {
-                stockLine.s_remote_cnt = s_remote_cnt + 1;
+                db.stock.s_remote_cnt[toUpdate] = s_remote_cnt + 1;
             } else {
-                stockLine.s_order_cnt = s_order_cnt + 1;
+                db.stock.s_order_cnt[toUpdate] = s_order_cnt + 1;
             }
-            db.stock.update(stockLine);
         }
         const auto tax = (Numeric<4, 4>(1.0) + w_tax + d_tax) * (Numeric<4, 4>(1.0) - c_discount);
         const auto price = Numeric<5, 2>(qty[index]) * i_price;
@@ -84,23 +83,21 @@ void delivery(int32_t w_id, int32_t o_carrier_id, Timestamp datetime) {
             return db.neworder.primaryHashIndex[{w_id, d_id, o_id}];
         }());
 
-        auto o = db.order.getRowForKey({w_id, d_id, o_id});
-        const auto o_ol_cnt = o.o_ol_cnt;
-        const auto o_c_id = o.o_c_id;
+        auto orderIndex = db.order.primaryHashIndex[ {w_id, d_id, o_id}];
+        const auto o_ol_cnt = db.order.o_ol_cnt[orderIndex];
+        const auto o_c_id = db.order.o_c_id[orderIndex];
 
-        o.o_carrier_id = o_carrier_id;
-        db.order.update(o);
+        db.order.o_carrier_id[orderIndex] = o_carrier_id;
 
         Numeric<6, 2> ol_total = 0;
         for (int ol_number = 1; Numeric<2, 0>(ol_number) < o_ol_cnt; ol_number += 1) {
-            auto ol = db.orderline.getRowForKey({w_id, d_id, o_id, ol_number});
-            const auto ol_amount = ol.ol_amount;
+            auto olIndex = db.orderline.primaryHashIndex[ {w_id, d_id, o_id, ol_number}];
+            const auto ol_amount = db.orderline.ol_amount[olIndex];
             ol_total = ol_total + ol_amount;
-            ol.ol_delivery_d = datetime;
-            db.orderline.update(ol);
+            db.orderline.ol_delivery_d[olIndex] = datetime;
         }
 
-        auto customer = db.customer.getRowForKey({w_id, d_id, o_c_id});
-        customer.c_balance = customer.c_balance + ol_total.castS<12>();
+        auto customerIndex = db.customer.primaryHashIndex[ {w_id, d_id, o_c_id}];
+        db.customer.c_balance[customerIndex] = db.customer.c_balance[customerIndex] + ol_total.castS<12>();
     }
 }
